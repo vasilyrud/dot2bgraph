8edce07351b6f141ab6a02921bdbb363695cd0f1
Vasily Rudchenko <vasilyrud@gmail.com>
2020-08-06 22:28:13 -0400

    Add nodes to regions

--- blockgraph/locations.py
+++ blockgraph/locations.py
@@ -18,25 +18,35 @@ import weakref
 
 from pygraphviz import AGraph
 
-def add_regions(cur_region: Region):
-    for sub_graph in cur_region.agraph.subgraphs():
-        sub_region = Region(sub_graph, cur_region)
+def add_regions_nodes(cur_region: Region):
+    cur_region.add_nodes(Node(node, cur_region) for node in direct_nodes(cur_region.agraph))
+    for sub_agraph in cur_region.agraph.subgraphs_iter():
+        sub_region = Region(sub_agraph, cur_region)
         cur_region.add_node(sub_region)
-        add_regions(sub_region)
+        add_regions_nodes(sub_region)
+
+def direct_nodes(agraph: AGraph) -> set:
+    all_nodes = set(agraph.nodes())
+    sub_agraph_nodes = set()
+    for sub_agraph in agraph.subgraphs_iter():
+        sub_agraph_nodes.update(sub_agraph.nodes())
+    return all_nodes - sub_agraph_nodes
 
 def dot2locations(dot: str):
     agraph = AGraph(string=dot)
 
-    print('VAS nodes')
-    print(agraph.nodes())
+    # print('VAS nodes')
+    # print(agraph.nodes())
 
-    print('VAS subg')
-    print(agraph.subgraphs()[0])
-    print(agraph.subgraphs()[0].subgraphs()[0])
+    # print('VAS subg')
+    # print(agraph.subgraphs()[0])
+    # print(agraph.subgraphs()[0].subgraphs()[0])
 
     base_region = Region(agraph)
-    add_regions(base_region)
+    add_regions_nodes(base_region)
 
+    # print(base_region)
+    base_region.print_nodes()
 
     # for all the Nodes in the base region:
     #     Generate node size recursively
@@ -50,12 +60,27 @@ def dot2locations(dot: str):
 
 class Node:
     def __init__(self, 
+        name: str,
         in_region: Optional[Region] = None,
         *args, **kwargs
     ):
+        self.name = name
         if in_region is not None:
             self.in_region = weakref.ref(in_region)
-        self.is_region = False
+        self.nodes = []
+
+    @property
+    def is_region(self) -> bool:
+        return len(self.nodes) > 0
+
+    def print_nodes(self, depth: int = 0):
+        print('{}{}'.format(depth*'  ', self))
+
+        for node in self.nodes:
+            node.print_nodes(depth+1)
+
+    def __repr__(self):
+        return '{} <{}>'.format(self.name, hex(id(self)))
 
 class Region(Node):
     def __init__(self,
@@ -64,11 +89,13 @@ class Region(Node):
         *args, **kwargs
     ):
         self.agraph = agraph
-        super().__init__(in_region)
-        self.nodes = []
-        self.is_region = True
+        super().__init__(agraph.get_name(), in_region)
 
-    def add_node(self, node: Node):
+    def add_nodes(self, nodes: Iterable[str]) -> None:
+        for node in nodes:
+            self.add_node(node)
+
+    def add_node(self, node: Node) -> None:
         self.nodes.append(node)
 
 class Locations:
