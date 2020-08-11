259ff1ebd31db07c05257749de6f461b4d7e7aad
Vasily Rudchenko <vasilyrud@gmail.com>
2020-08-10 20:18:11 -0400

    Add anodetonode type

--- blockgraph/locations.py
+++ blockgraph/locations.py
@@ -12,15 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict
+from typing import Dict, NewType
 
 from pygraphviz import AGraph
 
 from blockgraph.node import Node, Region
 
+ANodeToNode = NewType('ANodeToNode', Dict[str, Node])
+
 def add_regions_nodes(
     cur_region: Region,
-    anodes_to_nodes: Dict[str, Node],
+    anodes_to_nodes: ANodeToNode,
 ) -> None:
     for anode in direct_nodes(cur_region.agraph):
         node = Node(anode, cur_region)
@@ -43,7 +45,7 @@ def direct_nodes(agraph: AGraph) -> set:
 
 def add_edges(
     base_region: Region, 
-    anodes_to_nodes: Dict[str, Node],
+    anodes_to_nodes: ANodeToNode,
 ) -> None:
     for asource, adest in base_region.agraph.edges_iter():
         from_node = anodes_to_nodes[asource]
@@ -62,19 +64,16 @@ def dot2locations(dot: str):
     # print(agraph.subgraphs()[0])
     # print(agraph.subgraphs()[0].subgraphs()[0])
 
-    anodes_to_nodes = {}
+    anodes_to_nodes: ANodeToNode = {}
     base_region = Region(agraph)
     add_regions_nodes(base_region, anodes_to_nodes)
     add_edges(base_region, anodes_to_nodes)
 
-    # print(base_region)
     base_region.print_nodes()
 
-    # for all the Nodes in the base region:
-    #     Generate node size recursively
     # Determine sources and sinks
     # Determine cur_region depth
-    # Determin cur_region width
+    # Determine cur_region width
     # Place nodes in the region
     # Center nodes
 
