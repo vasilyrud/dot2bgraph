90740b2458246337f36e5a9c69b53645e787fd53
Vasily Rudchenko <vasilyrud@gmail.com>
2021-10-12 19:37:14 -0400

    Remove Python 3.10 from tests

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ DEPENDENCIES = [
     'yaspin',
 ]
 
-
 setup(
     name=NAME,
     version=VERSION,
@@ -67,6 +66,8 @@ setup(
 
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
 
